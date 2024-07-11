dddbf37b44e066dfd4078cbbf7215b1b79535729
cristobalHC <cristobalgabriel.hernandez@alumnos.ulagos.cl>
2024-07-11 11:14:14 -0400

    Add files via upload

---
+++ Ejercicio 1 tarea N°8.py
@@ -0,0 +1,4 @@
+def area_rectangulo(base, altura):
+    res=base*altura/2
+    return res
+print(area_rectangulo(10,15))
---
+++ Ejercicio 2 Tarea N°8.py
@@ -0,0 +1,4 @@
+def area_circulo(radio):
+    res=3.14159*radio*radio
+    return res
+print(area_circulo(5))
